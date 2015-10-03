38200a5befc53af53e1d090c67292830a28ea8b1
Javier Thaine <jthaine@cs.washington.edu>
2015-10-03 11:59:01 -0700

    Test changes

--- checker/tests/lock-safedefaults/BasicTest.java
+++ checker/tests/lock-safedefaults/BasicTest.java
@@ -23,13 +23,34 @@ public class BasicTest {
     void myAnnotatedMethod2() {
     }
 
+    ReentrantLock lockField = new ReentrantLock();
+    @GuardedBy("lockField") MyClass m;
+
+    Object o1 = new Object(), p1;
+
     @AnnotatedFor("lock")
     @MayReleaseLocks
-    void test() {
-        Object o = new Object(), p;
+    void testFields() {
         //:: error: (assignment.type.incompatible) :: error: (argument.type.incompatible)
-        p = myUnannotatedMethod(o);
-        myAnnotatedMethod(o);
+        p1 = myUnannotatedMethod(o1);
+        myAnnotatedMethod(o1);
+
+        // Now test that an unannotated method behaves as if it's annotated with @MayReleaseLocks
+        lockField.lock();
+        myAnnotatedMethod2();
+        m.field.toString();
+        myUnannotatedMethod2();
+        //:: error: (contracts.precondition.not.satisfied.field)
+        m.field.toString();
+    }
+
+    @AnnotatedFor("lock")
+    @MayReleaseLocks
+    void testLocalVariables() {
+        Object o2 = new Object(), p2;
+        //:: error: (assignment.type.incompatible) :: error: (argument.type.incompatible)
+        p2 = myUnannotatedMethod(o2);
+        myAnnotatedMethod(o2);
 
         // Now test that an unannotated method behaves as if it's annotated with @MayReleaseLocks
         ReentrantLock lock = new ReentrantLock();
